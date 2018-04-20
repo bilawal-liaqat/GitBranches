285e42e38327813a2793bcc2ad24ba52b00cc9fb
Bilawal Liaqat <bilawal.liaqat@nxb.com.pk>
2018-04-20 16:55:33 +0500

    ForgetVC added

---
+++ GitBranches/GitBranches/ForgetPasswordViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ForgetPasswordViewController.swift
+//  GitBranches
+//
+//  Created by Bilawal Liaqat on 20/04/2018.
+//  Copyright © 2018 Bilawal Liaqat. All rights reserved.
+//
+
+import UIKit
+
+class ForgetPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
