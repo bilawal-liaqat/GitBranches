ecc491c80d629c320e174e79e502518df16a1dcf
Muhammad Usman Tarar <usmantarar@usmantarar.local>
2018-04-20 17:33:45 +0500

    print lines added in LoginVC

--- GitBranches/GitBranches/LoginViewController.swift
+++ GitBranches/GitBranches/LoginViewController.swift
@@ -14,6 +14,11 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        
+        print("Usman")
+        print("Usman")
+        print("Usman")
     }
 
     override func didReceiveMemoryWarning() {
